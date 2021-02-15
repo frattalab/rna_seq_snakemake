0ea136a72b99a8f0d932f9b6299f57fc8e909da6
aleighbrown <brown.annaleigh@gmail.com>
2021-02-15 12:25:03 +0000

    more mem

--- submit_ena.sh
+++ submit_ena.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #Submit to the cluster, give it a unique name
 #$ -S /bin/bash
-#$ -l h_rt=72:0:0
 #$ -cwd
 #$ -V
-#$ -l h_vmem=2G,h_rt=20:00:00,tmem=2G
+#$ -l h_vmem=8G,h_rt=24:00:00,tmem=8G
 
 
 # join stdout and stderr output
